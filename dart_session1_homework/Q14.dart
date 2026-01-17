d47506ae9827f0cdb7105a34f1e79130fe480440
Mohamed Hassan <mohamedhassankamel9@gmail.com>
2026-01-17 18:08:11 +0200

    Dot operator (defrance between change by value and change by reference)

--- dart_session1_homework/Q14.dart
+++ dart_session1_homework/Q14.dart
@@ -2,7 +2,13 @@
 Write a Dart program that declares a String variable named greeting with an initial
 value of "Hello". Then, update greeting to "Hello, Dart!" and print it.*/
 void main() {
-  String greeting="Hello";//declare and assign initial value
-  greeting="Hello, Dart!";//update value
-  print(greeting);//print updated value
+  String greeting = "Hello"; //declare and assign initial value
+  greeting = "Hello, Dart!"; //update value
+  print(greeting); //print updated value
+  //defrance between change by value and change by reference
+  print(greeting.toUpperCase());//change by value not by reference
+  greeting = greeting.toUpperCase();//change by reference (overwriting the previous value)
+  print(greeting);
 }
+
+
