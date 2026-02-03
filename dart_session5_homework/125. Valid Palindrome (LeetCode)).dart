1d76b01de7ae6b4b44a85b6db0c0c606eb3a3f8b
Mohamed Hassan <mohamedhassankamel9@gmail.com>
2026-02-03 18:35:41 +0200

    refactor palindrome check for improved readability and efficiency

--- dart_session5_homework/125. Valid Palindrome (LeetCode)).dart
+++ dart_session5_homework/125. Valid Palindrome (LeetCode)).dart
@@ -1,34 +1,13 @@
 class Solution {
   bool isPalindrome(String s) {
-    String lowerCaseText = s.toLowerCase();
-    String cleanText = "";
-
-    for (int i = 0; i < lowerCaseText.length; i++) {
-      String char = lowerCaseText[i];
-
-      bool isLetter = char.compareTo('a') >= 0 && char.compareTo('z') <= 0;
-      bool isNumber = char.compareTo('0') >= 0 && char.compareTo('9') <= 0;
-
-      if (isLetter || isNumber) {
-        cleanText = cleanText + char;
+    s = s.toLowerCase();
+    String cleanS = s.replaceAll(RegExp(r'[^a-z0-9]'), '');
+    bool isPalindrome = true;
+    for (int i = 0; i < cleanS.length; i++) {
+      if (cleanS[i] != cleanS[cleanS.length - 1 - i]) {
+        isPalindrome = false;
       }
     }
-
-    String reversedText = "";
-    for (int i = cleanText.length - 1; i >= 0; i--) {
-      reversedText = reversedText + cleanText[i];
-    }
-
-    if (cleanText == reversedText) {
-      return true;
-    } else {
-      return false;
-    }
+    return isPalindrome;
   }
-}
-
-void main() {
-  Solution sol = Solution();
-  print(sol.isPalindrome("race a car"));
-  print(sol.isPalindrome("A man, a plan, a canal: Panama"));
 }
