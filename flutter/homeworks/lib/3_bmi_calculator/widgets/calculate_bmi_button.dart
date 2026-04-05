c0e0347f25bdb4b4a77f1a0d962efdda6d40c21f
Mohamed Hassan <mohamedhassankamel9@gmail.com>
2026-04-05 13:04:57 +0200

    Refactor CalculateBMIButton to use callback for onTap and clean up unused imports

--- flutter/homeworks/lib/3_bmi_calculator/widgets/calculate_bmi_button.dart
+++ flutter/homeworks/lib/3_bmi_calculator/widgets/calculate_bmi_button.dart
@@ -1,33 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:homeworks/3_bmi_calculator/Models/calculate_bmi.dart';
-import 'package:homeworks/3_bmi_calculator/result_screen.dart';
 
 class CalculateBMIButton extends StatelessWidget {
-  const CalculateBMIButton({super.key});
+  const CalculateBMIButton({super.key, required this.onTap});
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        Calculator(height: height, weight: weight).calcBMI();
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ResultScreen(bmiResult: 5)),
-        );
-      },
+      onTap: onTap,
       child: Container(
+        height: 80,
         width: double.infinity,
-        color: Color(0xffed0d54),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16),
-            child: Text(
-              'CALCULATE',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: .bold,
-                color: Colors.white,
-              ),
+        decoration: const BoxDecoration(
+          color: Color(0xffeb1555),
+        ),
+        child: const Center(
+          child: Text(
+            'CALCULATE',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
             ),
           ),
         ),
